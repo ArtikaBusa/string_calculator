aa39ad0531f8f01c9bde2fb2888040bc5790797e
Artika Busa <busaartika@gmail.com>
2025-09-29 12:00:26 +0530

    feat: return the number itself for single input

---
+++ app/services/calculator.rb
@@ -0,0 +1,7 @@
+class Calculator
+
+  def self.add(numbers)
+    return 0 if numbers.empty?
+    return numbers.to_i if numbers.size == 1
+  end
+end
---
+++ spec/services/calculator_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Calculator, type: :model do
+  describe ".add" do
+    it "returns 0 for empty string" do
+      expect(described_class.add("")).to eq(0)
+    end
+
+    it "returns the number itself for single input" do
+      expect(described_class.add("1")).to eq(1)
+    end
+
+  end
+end
