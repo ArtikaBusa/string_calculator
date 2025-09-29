c00b15f854bc71b2e34f62abea8deec0f6f52180
Artika Busa <busaartika@gmail.com>
2025-09-29 12:07:32 +0530

    feat: support custom delimiter syntax

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -2,7 +2,16 @@ class Calculator
 
   def self.add(numbers)
     return 0 if numbers.empty?
-    nums = numbers.split(/,|\n/).map(&:to_i)
+
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2..])
+      numbers = parts[1]
+    end
+
+    nums = numbers.split(/#{delimiter}/).map(&:to_i)
     nums.sum
   end
+
 end
--- spec/services/calculator_spec.rb
+++ spec/services/calculator_spec.rb
@@ -22,5 +22,8 @@ RSpec.describe Calculator, type: :model do
       expect(described_class.add("1\n2,3")).to eq(6)
     end
 
+    it "supports custom delimiter" do
+      expect(described_class.add("//;\n1;2")).to eq(3)
+    end
   end
 end
